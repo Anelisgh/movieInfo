dae415f0267b5c99cbdca8ff7c6b6cd9aa954ecd
anelisgh <anelis.gheorghias94@gmail.com>
2025-04-02 16:27:08 +0300

    Momentan nu e potrivit pentru render, trebuie schimbat cu postgresql

--- backend/src/main/java/com/example/movieInfo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/movieInfo/config/SecurityConfig.java
@@ -99,6 +99,4 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
-
 }
